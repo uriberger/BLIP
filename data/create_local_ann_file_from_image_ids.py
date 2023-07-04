928131a3111743432c226bb32fe50d69e8a56e92
Uri Berger <uriber@cs.huji.ac.il>
2023-07-04 14:17:26 +0300

    Added code for loading clip data

--- data/create_local_ann_file_from_image_ids.py
+++ data/create_local_ann_file_from_image_ids.py
@@ -2,10 +2,20 @@ import sys
 import json
 import random
 
-assert len(sys.argv) == 2
-image_ids_file = sys.argv[1]
+assert len(sys.argv) == 2 or len(sys.argv) == 3
+batch_ind = int(sys.argv[1])
+image_ids_file = 'reformulation_data/batch_' + str(batch_ind) + '/batch_' + str(batch_ind) + '_image_ids.json'
 with open(image_ids_file, 'r') as fp:
     image_id_list = json.load(fp)
+if len(sys.argv) == 3:
+    select_caption_method = sys.argv[2]
+    assert select_caption_method in ['random', 'clip'], 'Unknown select caption method ' + str
+else:
+    select_caption_method = 'random'
+
+if select_caption_method == 'clip':
+    with open('reformulation_data/batch_' + str(batch_ind) + '/image_id_to_caption_inds.json', 'r') as fp:
+        image_id_to_caption_inds = json.load(fp)
 
 ann_data = []
 
@@ -14,7 +24,10 @@ with open('../CLIP_prefix_caption/dataset_coco.json', 'r') as fp:
 
 for image_id in image_id_list:
     sample = [x for x in all_coco_data if x['cocoid'] == image_id][0]
-    caption_inds = random.sample(range(len(sample['sentences'])), 2)
+    if select_caption_method == 'random':
+        caption_inds = random.sample(range(len(sample['sentences'])), 2)
+    elif select_caption_method == 'clip':
+        caption_inds = image_id_to_caption_inds[str(image_id)]
     for caption_ind in caption_inds:
         res = {}
         res['image_id'] = image_id
